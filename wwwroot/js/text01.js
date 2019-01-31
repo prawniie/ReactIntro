a6e55d675d2e13d79aa186bfad4efae106c6b70d
rebeckalscarlsson <rebecka.ls.carlsson@gmail.com>
2019-01-31 13:41:48 +0100

    Gjort nästan hela text01

---
+++ wwwroot/js/text01.js
@@ -0,0 +1,68 @@
+﻿let root = document.getElementById("root");
+
+//Skapar fem input-fält med texten Peter. Ändras till Göran när man klickar i den.
+//class Input extends React.Component {
+
+//    state = { name: "Peter" }
+
+//    handleInput = () => {
+//        this.setState({ name: "Göran" });
+//    }
+
+//    render() {
+//        return (
+//            <input id="input" onInput={this.handleInput} value={this.state.name} ></input>
+//        )
+//    }
+//}
+
+//ReactDOM.render(<div><Input /><Input /><Input /><Input /><Input /></div>, root);
+
+//Skapar ett inputfält som kan ta in en parameter som värdet som skrivs ut, i detta fall hårdkodat
+//const FillTextbox = function (x) {
+//    console.log("x", x)
+
+//    return (
+//            <input value={x.minprop}/>
+//        );
+//}
+
+//let input = "hej";
+
+//ReactDOM.render(<FillTextbox minprop={input} />, root);
+
+class Input extends React.Component {
+
+    state = { name: "" }
+
+    handleChange = (event) => {
+        this.setState({ name: event.target.value });
+    }
+
+    render() {
+        return (
+            <div>
+            <input onChange={(this.handleChange)} value={this.state.name} ></input>
+            <input onChange={(this.handleChange)} value={this.state.name} ></input>
+            <input onChange={(this.handleChange)} value={this.state.name} ></input>
+            </div>
+        )
+    }
+}
+ReactDOM.render(<Input />, root);
+
+//handleInput = () => {
+//    <input value={this.state.name}></input>
+//}
+
+
+//render() {
+//    return (
+//        <div>
+//            <input onInput={this.handleInput} style={{ margin: '10px' }}>
+//            </input>
+//        </div>
+//    )
+//}
+
+
---
+++ wwwroot/js/tmp.js
@@ -0,0 +1,83 @@
+﻿console.log('Hello from tmp.js');
+
+let root = document.getElementById("root");
+
+class Button extends React.Component {
+
+    state = {counter: 0, nrmouseover: 0}
+
+    handleClick = () => {
+        this.setState((x) => {
+            return {
+                counter: x.counter + 1
+            };
+        })
+    }
+
+    handleMouseOver = () => {
+        this.setState((x) => {
+            return {
+                nrmouseover: x.nrmouseover + 1
+            };
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.handleClick} onMouseOver={this.handleMouseOver} style={{ margin: '10px' }}>
+                    Antal klick {this.state.counter}
+                    --------------------------------------
+                    Antal mouseover {this.state.nrmouseover}
+                </button>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<div><Button /><Button /></div>, root);
+
+
+//----------------------------------------------------------
+
+//const Kalle = function (props) {
+//    return (
+//        <div>
+//            <h3>{props.minprop}</h3>
+//            <button>{props.minprop}</button>
+//            </div>
+//        );
+//}
+
+//ReactDOM.render(<Kalle minprop="kör" />, root);
+
+
+
+//-----------------------------------------------------------------------
+
+//const Knapp = function () {
+//    return (
+//        <button>Goooo</button>
+//    );
+//}
+
+//ReactDOM.render(
+//    <div>
+//        <Knapp />
+//        <Knapp />
+//    </div> , root);
+
+
+//----------------------------------------------------------------------
+
+//OBS KAN EJ blanda stora och små bokstäver måste skriva med små bokstäver <div> inte <Div>
+//Men react-Element ska man skriva med stor bokstav tex Knapp
+
+//OBS render vill bara ha ett objekt så då måste man lägga flera grejer i en grej så blir den nöjd
+//Flera render efter sig --> kommer bara visa från den sista
+
+//ReactDOM.render(
+//    <div>
+//        <p>Hello World</p>
+//        <p>Hejhej</p>
+//    </div>, root);
