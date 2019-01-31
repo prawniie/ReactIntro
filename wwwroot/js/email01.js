45974425d47565e3bd8e5b7622f421aa3779d4cd
rebeckalscarlsson <rebecka.ls.carlsson@gmail.com>
2019-01-31 14:22:38 +0100

    Initial commit email01 uppgift

---
+++ wwwroot/js/email01.js
@@ -0,0 +1,19 @@
+﻿let root = document.getElementById("root");
+
+class Email extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <form>
+                    Förnamn: <input></input>
+                    Efternamn: <input></input>
+                    Email: <input></input>
+                </form>
+            </div>
+            )
+    }
+
+}
+
+ReactDOM.render(<Email />, root);
